9d0ee344bcbb22bcc103ed492ba5de9066c61abd
krkonsap <42966629+krkonsap@users.noreply.github.com>
2018-12-18 13:54:25 +0200

    18.12

--- js_17.12/main.js
+++ js_17.12/main.js
@@ -4,9 +4,16 @@ const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
+//mängu resetimiseks
+const guessReset = document.querySelector('.guessreset');
+//
+
 let randomNumber = Math.floor(Math.random()*100)+1;
 
 let guessCount = 1;
+
+const lowOrHighText = 'Viimane pakkumine on ';
+
 
 const checkGuess = () => {
   /*console.log('Vajutasid nuppu');*/
@@ -18,19 +25,50 @@ const checkGuess = () => {
   
   guesses.textContent += `${userGuess}, `;
   
-  if (userguess === randomNumber) {
+  if (userGuess === randomNumber) {
     // siis kui sisestatakse õige number
+    lastResult.textContent = 'Õige vastus boiii';
+    lastResult.style.backgroundColor = 'green';
+    lastResult.style.fontSize = '30px';
+    lowOrHigh.textContent = '';
+    
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
   } else if (guessCount === 10) {
     // kui arvamiste korrad saavad otsa
+    lastResult.textContent = 'Kõik. Mäng läbi.';
+    lastResult.style.backgroundColor = 'red';
+    lowOrHigh.textContent = '';
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
   } else {
+    lastResult.textContent = 'Vale vastus!!';
+    lastResult.style.backgroundColor = 'orange';
+    
     if(userGuess < randomNumber) {
       //liiga madal
-    } else if (userGuide > randomNumber) {
+      lowOrHigh.textContent = lowOrHighText + 'liiga madal.';
+    } else if (userGuess > randomNumber) {
       //liiga kõrge
+      lowOrHigh.textContent = lowOrHighText + 'liiga kõrge.';
     };
   };
   
   guessCount++;
+  guessField.value = '';
+  guessField.focus();
+   
 };
+
+//resetimisfunktsioon
+const resetimine = () => {
+  guessCount.value = 1;
+  lowOrHigh.textContent = '';
+  guesses.textContent = '';
+  lastResult.textContent = '';
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+};
+
 
 guessSubmit.addEventListener('click', checkGuess);
