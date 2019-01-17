4d6660000bb81233ceb4bd725e4b9d48dc6dcba8
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-17 12:53:31 +0200

    lisatud slaidishow avalehele

--- varjupaik/js/main.js
+++ varjupaik/js/main.js
@@ -1,3 +1,4 @@
+// NAVBAR
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +7,21 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+// NAVBAR
+
+// SLAIDID
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slaidid");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
+// SLAIDID
